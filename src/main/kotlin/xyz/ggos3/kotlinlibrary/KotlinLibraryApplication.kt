9b6099eb2f8ed25cbee3208a2a5a5fdbe2963279
GGos3 <janpll0421@gmail.com>
2023-05-25 14:05:41 +0900

    chore: init

---
+++ src/main/kotlin/xyz/ggos3/kotlinlibrary/KotlinLibraryApplication.kt
@@ -0,0 +1,11 @@
+package xyz.ggos3.kotlinlibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinLibraryApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinLibraryApplication>(*args)
+}
