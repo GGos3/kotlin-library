e5995f789e4cb4f9d07d9144000a2653c384d93d
GGos3 <janpll0421@gmail.com>
2023-05-25 18:52:32 +0900

    feat: UserService 추가

---
+++ src/main/kotlin/xyz/ggos3/kotlinlibrary/service/user/UserService.kt
@@ -0,0 +1,40 @@
+package xyz.ggos3.kotlinlibrary.service.user
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import xyz.ggos3.kotlinlibrary.domain.user.User
+import xyz.ggos3.kotlinlibrary.domain.user.UserRepository
+import xyz.ggos3.kotlinlibrary.dto.user.request.UserCreateRequest
+import xyz.ggos3.kotlinlibrary.dto.user.request.UserUpdateRequest
+import xyz.ggos3.kotlinlibrary.dto.user.response.UserResponse
+
+@Service
+class UserService(
+    private val userRepository: UserRepository
+) {
+    @Transactional
+    fun saveUser(request: UserCreateRequest) {
+        val user = User(request.name, request.age)
+        userRepository.save(user)
+    }
+
+    @Transactional(readOnly = true)
+    fun getUser(): List<UserResponse> {
+        return userRepository.findAll()
+            .map { user -> UserResponse(user) }
+    }
+
+    @Transactional
+    fun updateUserName(request: UserUpdateRequest) {
+        val user = userRepository.findById(request.id)
+            .orElseThrow(::IllegalArgumentException)
+        user.updateName(request.name)
+    }
+
+    @Transactional
+    fun deleteUser(name: String) {
+        val user = userRepository.findByName(name)
+            ?: throw IllegalArgumentException()
+        userRepository.delete(user)
+    }
+}
