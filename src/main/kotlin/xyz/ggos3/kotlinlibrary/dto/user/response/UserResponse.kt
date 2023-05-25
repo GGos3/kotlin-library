e0b1f3a7d71ffaeb5c9fea2097a2c9dc92707442
GGos3 <janpll0421@gmail.com>
2023-05-25 18:52:40 +0900

    feat: DTO 수정

--- src/main/kotlin/xyz/ggos3/kotlinlibrary/dto/user/response/UserResponse.kt
+++ src/main/kotlin/xyz/ggos3/kotlinlibrary/dto/user/response/UserResponse.kt
@@ -1,7 +1,11 @@
 package xyz.ggos3.kotlinlibrary.dto.user.response
 
+import xyz.ggos3.kotlinlibrary.domain.user.User
+
 data class UserResponse (
-    val id: Long,
-    val name: String,
-    val age: Int?
-)
+    val user: User
+) {
+    val id: Long = user.id!!
+    val name: String = user.name
+    val age: Int? = user.age
+}
