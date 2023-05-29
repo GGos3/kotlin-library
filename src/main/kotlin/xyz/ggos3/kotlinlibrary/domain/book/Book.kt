d74f40deba8b264ffc577be9b1d7dad8bcb2b2f3
GGos3 <janpll0421@gmail.com>
2023-05-29 18:05:17 +0900

    feat: fixture 함수 추가, type 추가

--- src/main/kotlin/xyz/ggos3/kotlinlibrary/domain/book/Book.kt
+++ src/main/kotlin/xyz/ggos3/kotlinlibrary/domain/book/Book.kt
@@ -9,6 +9,8 @@ import jakarta.persistence.Id
 class Book (
     val name: String,
 
+    val type: String,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
@@ -16,5 +18,19 @@ class Book (
     init {
         if (name.isBlank())
             throw IllegalArgumentException("이름은 비어 있을 수 없습니다.")
+    }
+
+    companion object {
+        fun fixture(
+            name: String = "책 이름",
+            type: String = "COMPUTER",
+            id: Long? = null
+        ): Book {
+            return Book(
+                name = name,
+                type = type,
+                id = id
+            )
+        }
     }
 }
