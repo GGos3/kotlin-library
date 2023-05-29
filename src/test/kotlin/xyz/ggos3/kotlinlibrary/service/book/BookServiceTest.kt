0efc887c2474b6e402c08715ca0cee57cea0615d
GGos3 <janpll0421@gmail.com>
2023-05-29 22:46:38 +0900

    test: BookService 테스트 코드 생성

---
+++ src/test/kotlin/xyz/ggos3/kotlinlibrary/service/book/BookServiceTest.kt
@@ -0,0 +1,85 @@
+package xyz.ggos3.kotlinlibrary.service.book
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.boot.test.context.SpringBootTest
+import xyz.ggos3.kotlinlibrary.domain.book.Book
+import xyz.ggos3.kotlinlibrary.domain.book.BookRepository
+import xyz.ggos3.kotlinlibrary.domain.book.BookType
+import xyz.ggos3.kotlinlibrary.domain.user.User
+import xyz.ggos3.kotlinlibrary.domain.user.UserRepository
+import xyz.ggos3.kotlinlibrary.domain.user.loanhistory.UserLoanHistory
+import xyz.ggos3.kotlinlibrary.domain.user.loanhistory.UserLoanHistoryRepository
+import xyz.ggos3.kotlinlibrary.domain.user.loanhistory.UserLoanStatus
+import xyz.ggos3.kotlinlibrary.dto.book.request.BookLoanRequest
+import xyz.ggos3.kotlinlibrary.dto.book.request.BookRequest
+import java.lang.IllegalArgumentException
+
+@SpringBootTest
+class BookServiceTest(
+    private val bookService: BookService,
+    private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository
+) {
+
+    @AfterEach
+    fun clean() {
+        bookRepository.deleteAll()
+        userRepository.deleteAll()
+    }
+
+    @Test
+    @DisplayName("책 등록이 정상 동작한다.")
+    fun saveBookTest() {
+        // given
+        val request = BookRequest("이상한 나라의 엘리스", BookType.COMPUTER)
+
+        // when
+        bookService.saveBook(request)
+
+        // then
+        val books = bookRepository.findAll()
+        assertThat(books).hasSize(1)
+        assertThat(books[0].name).isEqualTo("이상한 나라의 엘리스")
+        assertThat(books[0].type).isEqualTo(BookType.COMPUTER)
+    }
+
+    @Test
+    @DisplayName("책 대출이 정상 동작한다")
+    fun loanBookTest() {
+        // given
+        bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
+        val savedUser = userRepository.save(User("김장필", 19))
+        val request = BookLoanRequest("김장필", "이상한 나라의 엘리스")
+
+        // when
+        bookService.loanBook(request)
+
+        // then
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("이상한 나라의 엘리스")
+        assertThat(results[0].user.id).isEqualTo(savedUser.id)
+        assertThat(results[0].status).isEqualTo(UserLoanStatus.LOANED)
+    }
+
+    @Test
+    @DisplayName("책이 이미 대출되어 있다면, 신규 대출이 실패한다.")
+    fun loanBookFailTest() {
+        // given
+        bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
+        val savedUser = userRepository.save(User("김장필", 19))
+        userLoanHistoryRepository.save(UserLoanHistory.fixture(savedUser))
+        val request = BookLoanRequest("김장필", "이상한 나라의 엘리스")
+
+        // when & then
+        val message = assertThrows<IllegalArgumentException> {
+            bookService.loanBook(request)
+        }.message
+        assertThat(message).isEqualTo("이미 대출되어 있는 책입니다.")
+    }
+}
