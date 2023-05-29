7142daa0a8ce3c0977958daa4e91cf0d6746bda4
GGos3 <janpll0421@gmail.com>
2023-05-29 21:58:55 +0900

    feat: Enum Type 추가

--- src/main/kotlin/xyz/ggos3/kotlinlibrary/dto/book/request/BookRequest.kt
+++ src/main/kotlin/xyz/ggos3/kotlinlibrary/dto/book/request/BookRequest.kt
@@ -1,6 +1,8 @@
 package xyz.ggos3.kotlinlibrary.dto.book.request
 
+import xyz.ggos3.kotlinlibrary.domain.book.BookType
+
 data class BookRequest (
     val name: String,
-    val type: String
+    val type: BookType
 )
